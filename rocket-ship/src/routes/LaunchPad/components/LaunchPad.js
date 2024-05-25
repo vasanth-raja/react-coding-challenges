122f78992fa1d02aa565e0fa1c2256d8bd417d60
vasanth-raja <vasanthrajark@gmail.com>
2024-05-25 20:47:49 +0530

    rocket ship

--- rocket-ship/src/routes/LaunchPad/components/LaunchPad.js
+++ rocket-ship/src/routes/LaunchPad/components/LaunchPad.js
@@ -3,11 +3,12 @@ import { ClassRocket, FunctionalRocket } from './Rocket';
 import '../styles/_launchpad.scss';
 
 export default function LaunchPad() {
-  const [, triggerRerender] = useState(Date.now());
+  // const [, triggerRerender] = useState(Date.now());
   
   useEffect(() => {
-    setInterval(() => { triggerRerender(Date.now()); }, 500);
+    // setInterval(() => { triggerRerender(Date.now()); }, 500);
   }, [])
+   console.log("rerendered")                       
   
   return (
     <div className="launchpad">
